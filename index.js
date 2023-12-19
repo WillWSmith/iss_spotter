714ecdf45d3d18c8cfda0642fabb2870a7fb4792
Will Smith <will.w.smith94@gmail.com>
2023-12-19 10:46:37 -0500

    Now prints the date and time and for how long the next passover will be

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require(`./iss`);
+const { nextISSTimesForMyLocation } = require(`./iss`);
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!", error);
-    return;
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
   }
+};
 
-  console.log(`It worked! Returned IP:`, ip);
-
-  fetchCoordsByIP(ip, (error, coords) => {
-    if (error) {
-      console.log(`Error:`, error);
-      return;
-    }
-
-    console.log(`Location:`, coords);
-
-    fetchISSFlyOverTimes(coords, (error, flyoverTimes) => {
-      if (error) {
-        console.log(`Error fetching ISS flyover times:`, error);
-        return;
-      }
-
-      console.log(`ISS Flyover Times:`, flyoverTimes);
-    });
-  });
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  
+  printPassTimes(passTimes);
 });
